7d41a13cda54021b3f8d64a2cb75718da8878b5c
gleniomontovani <gleniomontovani@gmail.com>
2024-04-04 11:01:03 -0300

    Commit inicial

---
+++ src/main/java/br/com/postech/techchallenge/microservico/producao/TechchallengeMicroservicoProducaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallenge.microservico.producao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallengeMicroservicoProducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechchallengeMicroservicoProducaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postech/techchallenge/microservico/producao/TechchallengeMicroservicoProducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallenge.microservico.producao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallengeMicroservicoProducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
